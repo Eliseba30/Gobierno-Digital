7c9d5eafcfa5d327931f2a3405ef1f3fddb80725
Eliseba30 <127545848+Eliseba30@users.noreply.github.com>
2023-03-13 23:48:31 -0600

    Av

---
+++ poke2023/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Infopokemon from './components/Pikachu';
+
+function App() {
+  return (
+    <div className="App">
+      <Infopokemon/>
+    </div>
+  );
+}
+
+export default App;
